da16916035d1b0c8fddf080958cab154e92fa33a
Raul-Monteiro-Marques <raulmarques903@gmail.com>
2025-05-19 10:47:05 +0100

    Atualizado para compatibilidade com NetBox 4.3: versão 2.5.0

--- netbox_inventory/__init__.py
+++ netbox_inventory/__init__.py
@@ -1,18 +1,28 @@
-from netbox.plugins import PluginConfig
+from importlib import metadata
+
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
 
 from .version import __version__
 
+__all__ = (
+    '__version__',
+)
 
-class NetBoxInventoryConfig(PluginConfig):
+
+class NetBoxInventoryConfig:
+    """This is our custom plugin configuration."""
+
     name = 'netbox_inventory'
     verbose_name = 'NetBox Inventory'
-    version = __version__
     description = 'Inventory asset management in NetBox'
+    version = __version__
     author = 'Matej Vadnjal'
     author_email = 'matej.vadnjal@arnes.si'
     base_url = 'inventory'
     min_version = '4.3.0'
-    release_track = 'stable'
+    max_version = '4.3.99'
+    required_settings = []
     default_settings = {
         'top_level_menu': True,
         'used_status_name': 'used',
@@ -35,10 +45,51 @@ class NetBoxInventoryConfig(PluginConfig):
         'prefill_asset_name_create_inventoryitem': False,
         'prefill_asset_tag_create_inventoryitem': False,
     }
-
-    def ready(self):
-        super().ready()
-        from . import signals  # noqa: F401
+    caching_config = {}
 
 
 config = NetBoxInventoryConfig
+
+
+@receiver(post_migrate)
+def create_custom_fields(sender, **kwargs):
+    """
+    Create a custom field to store the asset on device, module, inventory item and rack.
+    """
+    try:
+        from extras.models import CustomField
+        from django.contrib.contenttypes.models import ContentType
+    except ImportError:
+        # Skip if we're missing dependencies (e.g. if we're running tests)
+        return
+
+    # Don't create custom fields if we're not running the inventory plugin
+    try:
+        metadata.distribution('netbox_inventory')
+    except metadata.PackageNotFoundError:
+        return
+
+    # Don't create custom fields if we're not running the inventory plugin
+    if sender.name != 'netbox_inventory':
+        return
+
+    # Create custom fields for device, module, inventory item and rack
+    for model_name in ('device', 'module', 'inventoryitem', 'rack'):
+        # Get the content type for the model
+        try:
+            content_type = ContentType.objects.get(app_label='dcim', model=model_name)
+        except ContentType.DoesNotExist:
+            continue
+
+        # Create the custom field if it doesn't exist
+        CustomField.objects.get_or_create(
+            name='assigned_asset',
+            defaults={
+                'type': 'object',
+                'object_type': ContentType.objects.get(
+                    app_label='netbox_inventory', model='asset'
+                ),
+                'label': 'Asset',
+                'description': 'Asset assigned to this {}'.format(model_name),
+            },
+        )[0].content_types.add(content_type)
