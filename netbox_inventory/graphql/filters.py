fb3247912f32d3ed29e6bb2a24c9505636426597
Raul-Monteiro-Marques <raulmarques903@gmail.com>
2025-05-19 11:00:00 +0100

    Corrigido erro AttributeError: module strawberry_django.filters has no attribute StringFilter para compatibilidade com NetBox 4.3

--- netbox_inventory/graphql/filters.py
+++ netbox_inventory/graphql/filters.py
@@ -18,10 +18,10 @@ __all__ = (
 @strawberry_django.filter(models.Asset, lookups=True)
 class AssetFilter(BaseFilterMixin):
     # Filtros específicos para Asset
-    name = strawberry_django.filters.StringFilter()
-    serial = strawberry_django.filters.StringFilter()
-    asset_tag = strawberry_django.filters.StringFilter()
-    status = strawberry_django.filters.StringFilter()
+    name = strawberry_django.filters.FilterField(lookups=True)
+    serial = strawberry_django.filters.FilterField(lookups=True)
+    asset_tag = strawberry_django.filters.FilterField(lookups=True)
+    status = strawberry_django.filters.FilterField(lookups=True)
     
     # Filtros para relacionamentos
     device_type = strawberry_django.filters.FilterField(lookups=True)
@@ -42,42 +42,42 @@ class AssetFilter(BaseFilterMixin):
 
 @strawberry_django.filter(models.Supplier, lookups=True)
 class SupplierFilter(BaseFilterMixin):
-    name = strawberry_django.filters.StringFilter()
-    slug = strawberry_django.filters.StringFilter()
-    description = strawberry_django.filters.StringFilter()
+    name = strawberry_django.filters.FilterField(lookups=True)
+    slug = strawberry_django.filters.FilterField(lookups=True)
+    description = strawberry_django.filters.FilterField(lookups=True)
 
 
 @strawberry_django.filter(models.Purchase, lookups=True)
 class PurchaseFilter(BaseFilterMixin):
-    name = strawberry_django.filters.StringFilter()
-    status = strawberry_django.filters.StringFilter()
-    date = strawberry_django.filters.DateFilter()
-    description = strawberry_django.filters.StringFilter()
+    name = strawberry_django.filters.FilterField(lookups=True)
+    status = strawberry_django.filters.FilterField(lookups=True)
+    date = strawberry_django.filters.FilterField(lookups=True)
+    description = strawberry_django.filters.FilterField(lookups=True)
     supplier = strawberry_django.filters.FilterField(lookups=True)
 
 
 @strawberry_django.filter(models.Delivery, lookups=True)
 class DeliveryFilter(BaseFilterMixin):
-    name = strawberry_django.filters.StringFilter()
-    date = strawberry_django.filters.DateFilter()
-    description = strawberry_django.filters.StringFilter()
+    name = strawberry_django.filters.FilterField(lookups=True)
+    date = strawberry_django.filters.FilterField(lookups=True)
+    description = strawberry_django.filters.FilterField(lookups=True)
     purchase = strawberry_django.filters.FilterField(lookups=True)
     receiving_contact = strawberry_django.filters.FilterField(lookups=True)
 
 
 @strawberry_django.filter(models.InventoryItemType, lookups=True)
 class InventoryItemTypeFilter(BaseFilterMixin):
-    model = strawberry_django.filters.StringFilter()
-    slug = strawberry_django.filters.StringFilter()
-    part_number = strawberry_django.filters.StringFilter()
-    description = strawberry_django.filters.StringFilter()
+    model = strawberry_django.filters.FilterField(lookups=True)
+    slug = strawberry_django.filters.FilterField(lookups=True)
+    part_number = strawberry_django.filters.FilterField(lookups=True)
+    description = strawberry_django.filters.FilterField(lookups=True)
     manufacturer = strawberry_django.filters.FilterField(lookups=True)
     inventoryitem_group = strawberry_django.filters.FilterField(lookups=True)
 
 
 @strawberry_django.filter(models.InventoryItemGroup, lookups=True)
 class InventoryItemGroupFilter(BaseFilterMixin):
-    name = strawberry_django.filters.StringFilter()
-    slug = strawberry_django.filters.StringFilter()
-    description = strawberry_django.filters.StringFilter()
+    name = strawberry_django.filters.FilterField(lookups=True)
+    slug = strawberry_django.filters.FilterField(lookups=True)
+    description = strawberry_django.filters.FilterField(lookups=True)
     parent = strawberry_django.filters.FilterField(lookups=True)
